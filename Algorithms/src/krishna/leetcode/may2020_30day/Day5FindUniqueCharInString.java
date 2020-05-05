5b74203572b26b70e2cd7bd354b42406269ca8ec
nits.krishna <nits.krishna@gmail.com>
2020-05-05 13:30:08 +0530

    Day 5 Leetcode May 2020 , 30 day challenge , First Unique character in string

---
+++ Algorithms/src/krishna/leetcode/may2020_30day/Day5FindUniqueCharInString.java
@@ -0,0 +1,36 @@
+package krishna.leetcode.may2020_30day;
+
+/**
+ * Given a string, find the first non-repeating character in it and return it's
+ * index. If it doesn't exist, return -1.
+ * 
+ * Examples:
+ * 
+ * s = "leetcode" return 0.
+ * 
+ * s = "loveleetcode", return 2.
+ * 
+ * 
+ * Note: You may assume the string contain only lowercase letters.
+ * 
+ * @author krishna_k
+ *
+ */
+public class Day5FindUniqueCharInString {
+	public int firstUniqChar(String s) {
+		int[] marker = new int[26];
+		int len = s.length();
+		char[] arr = s.toCharArray();
+		for (char c : arr) {
+			marker[c - 'a']++;
+		}
+		int i = 0;
+		for (char c : arr) {
+			if (marker[c - 'a'] == 1) {
+				return i;
+			}
+			i++;
+		}
+		return -1;
+	}
+}
